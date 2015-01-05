ff4594dcfb15eab666beefd76e8aa10a03f7105f
Sarah Pope <spope@indatus.com>
2015-01-05 17:10:07 -0500

    home page styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,19 +125,7 @@ module.exports = function (grunt) {
       server: '.tmp'
     },
 
-    // Make sure code styles are up to par and there are no obvious mistakes
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      },
-      all: [
-        'Gruntfile.js',
-        '<%= config.app %>/scripts/{,*/}*.js',
-        '!<%= config.app %>/scripts/vendor/*',
-        'test/spec/{,*/}*.js'
-      ]
-    },
+    
 
     // Mocha testing framework configuration options
     mocha: {
